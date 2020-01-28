b7eee8047ce974b627843ffcb206ac71a9d14d9b
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-01-29 01:12:22 +0700

    Create loadbalance.sh

---
+++ loadbalance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+        sudo apt install software-properties-common
+        sudo add-apt-repository -y  ppa:nginx/stable
+        sudo apt-get update
+        sudo apt-get install -y nginx
+        sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+        sudo mv /Project-Cilsy-AWS/nginx/default.loadbalance /etc/nginx/sites-available/default
+        sudo nginx -t
+        sudo nginx -s reload
+        sudo systemctl restart nginx.service
