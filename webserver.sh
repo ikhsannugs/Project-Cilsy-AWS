f432b44c2fa918a3801a7b95824a647e1de1a867
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-01-28 23:42:05 +0700

    Create webserver.sh

---
+++ webserver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+        sudo apt install software-properties-common
+        sudo add-apt-repository -y  ppa:nginx/stable
+        sudo apt-get update
+        sudo apt-get install -y nginx php7.2-fpm
+        
