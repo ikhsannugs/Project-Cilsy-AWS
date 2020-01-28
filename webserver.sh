35bcd9e38aafa184b7f243d1c879ba21582695a9
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-01-28 23:49:51 +0700

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -2,6 +2,6 @@
         sudo apt install software-properties-common
         sudo add-apt-repository -y  ppa:nginx/stable
         sudo apt-get update
-        sudo apt-get install -y nginx php7.2-fpm
+        sudo apt-get install -y nginx php7.2-fpm git
         
         
