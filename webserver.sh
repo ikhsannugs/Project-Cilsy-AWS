a1a18d595b0a7ab48593b94ac3d9a035b74745d9
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-01-29 00:01:09 +0700

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -3,5 +3,7 @@
         sudo add-apt-repository -y  ppa:nginx/stable
         sudo apt-get update
         sudo apt-get install -y nginx php7.2-fpm git
-        
+        sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+        sudo mv /home/ubuntu/Project-Cilsy-AWS/nginx/default.webserver /etc/nginx/sites-available/default
+
         
