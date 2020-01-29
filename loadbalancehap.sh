d581db3beff9ab35901d37ee8ae97411cf7a2c5c
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-01-29 12:17:50 +0700

    Update loadbalancehap.sh

--- loadbalancehap.sh
+++ loadbalancehap.sh
@@ -2,5 +2,6 @@
         sudo apt install software-properties-common
         sudo apt-get update
         sudo apt-get install -y haproxy
-        sudo vim /etc/haproxy/haproxy.cfg
+        sudo mv /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.bak
+        sudo mv /Project-Cilsy-AWS-master/nginx/haproxy.cfg.set /etc/haproxy/haproxy.cfg
         sudo systemctl restart haproxy.service 
