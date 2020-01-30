b356c78762fecb6e2912a6fd06a75dd4278a91a4
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-01-30 14:11:56 +0700

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -2,7 +2,7 @@
         sudo apt install software-properties-common
         sudo add-apt-repository -y  ppa:nginx/stable
         sudo apt-get update
-        sudo apt-get install -y nginx php7.2-fpm git php7.2-mysql
+        sudo apt-get install -y nginx php7.2-fpm git php7.2-mysql mysql-client-5.7
         sudo cp -r /Project-Cilsy-AWS-master/sosial-media-master/* /var/www/html
         sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
         sudo mv /Project-Cilsy-AWS-master/nginx/default.web /etc/nginx/sites-available/default
