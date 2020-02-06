4083f4c519b70164e32625c8b688ee780bf5f786
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-02-06 13:14:42 +0700

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -3,12 +3,6 @@
         sudo add-apt-repository -y  ppa:nginx/stable
         sudo apt-get update
         sudo apt-get install -y nginx php7.2-fpm git php7.2-mysql mysql-client-5.7
-        #config.php
-        sudo sed -i 's/username/admin/g' /Project-Cilsy-AWS-master/sosial-media-master/config.php   
-        sudo sed -i 's/password/abcd1234567890/g' /Project-Cilsy-AWS-master/sosial-media-master/config.php   
-        sudo sed -i 's/database/pesbukdb/g' /Project-Cilsy-AWS-master/sosial-media-master/config.php 
-        sudo sed -i 's/alamat/database-1.citrm16v9p2y.ap-southeast-1.rds.amazonaws.com/g'  /Project-Cilsy-AWS-master/sosial-media-master/config.php  
-        sed -i 's/\r$//g' /Project-Cilsy-AWS-master/sosial-media-master/config.php
         sudo cp -r /Project-Cilsy-AWS-master/sosial-media-master/* /var/www/html
         sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
         sudo mv /Project-Cilsy-AWS-master/nginx/default.web /etc/nginx/sites-available/default
